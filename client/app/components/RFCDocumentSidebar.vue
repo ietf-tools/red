93b57ba4acbb1b9075609c169314ff36378154db
Matthew Holloway <matthew@staff.ietf.org>
2025-08-18 18:46:25 +1200

    feat: unifying rfbtabs mobile/desktop

--- client/app/components/RFCDocumentSidebar.vue
+++ client/app/components/RFCDocumentSidebar.vue
@@ -23,39 +23,16 @@
               <GraphicsExpandSidebar class="inline-block mr-1 rotate-180" />
             </button>
           </RFCMobileBanner>
-          <div class="pl-4 pr-2 bg-white dark:bg-blue-900">
+          <div class="bg-white dark:bg-blue-900">
             <RFCTabs
               ref="mobileRFCTabs"
               v-model="selectedTab"
               :rfc="props.rfc"
-              :rfc-bucket-html-doc="props.rfcBucketHtmlDocument"
+              :is-mobile="true"
+              :rfc-bucket-html-document="props.rfcBucketHtmlDocument"
               :has-table-of-contents="props.hasTableOfContents"
-            >
-              <template #slot0>
-                <TableOfContents
-                  v-if="props.rfcBucketHtmlDocument.tableOfContents"
-                  :toc="props.rfcBucketHtmlDocument.tableOfContents"
-                  list-type="ordered"
-                  wrapper-class="flex flex-col min-h-0 pt-4 pb-2 px-4"
-                  list-class="mt-2 mr-1 pl-0 -ml-1"
-                  nested-list-class="pl-2"
-                  :list-item-class="`block text-sm py-2 dark:border-t-gray-500 ${ANCHOR_TAILWIND_STYLE}`"
-                  links-active-class="text-shadow-bold"
-                  link-class="block no-underline hover:underline"
-                  last-link-class="flex-1"
-                >
-                  <Heading
-                    level="2"
-                    style-level="5"
-                    class="mt-4 mb-1 sr-only"
-                  >
-                    In this section
-                  </Heading>
-                </TableOfContents>
-              </template>
-            </RFCTabs>
+            />
           </div>
-
           <DialogClose />
         </DialogContent>
       </DialogPortal>
@@ -66,179 +43,10 @@
         ref="desktopRFCTabs"
         v-model="selectedTab"
         :rfc="props.rfc"
-        :rfc-bucket-html-doc="props.rfcBucketHtmlDocument"
+        :is-mobile="true"
+        :rfc-bucket-html-document="props.rfcBucketHtmlDocument"
         :has-table-of-contents="props.hasTableOfContents"
-      >
-        <template #slot0>
-          <TableOfContentsHighlight
-            v-if="props.rfcBucketHtmlDocument.tableOfContents"
-            :toc="props.rfcBucketHtmlDocument.tableOfContents"
-            list-type="ordered"
-            wrapper-class="flex flex-col min-h-0 pt-4 pb-2 px-4"
-            list-class="mr-1"
-            nested-list-class="pl-2"
-            :links-class="`block text-sm py-2 dark:border-t-gray-500 ${ANCHOR_TAILWIND_STYLE}`"
-            links-active-class="text-shadow-bold"
-            link-class="block no-underline hover:underline"
-            last-link-class="flex-1"
-          >
-            <Heading
-              level="2"
-              style-level="5"
-              class="mt-4 mb-1 sr-only"
-            >
-              In this section
-            </Heading>
-          </TableOfContentsHighlight>
-        </template>
-        <template #slot1>
-          <Heading
-            level="3"
-            style-level="4"
-            class="mt-4"
-          >
-            Details
-          </Heading>
-          <dl class="text-sm">
-            <template v-if="rfc.updates && rfc.updates.length > 0">
-              <dt class="font-bold mt-2">Updates ({{ rfc.updates.length }})</dt>
-              <dd>
-                <span
-                  v-for="(update, updateIndex) in props.rfc.updates"
-                  :key="updateIndex"
-                >
-                  <RFCRouterLink :href="infoRfcPathBuilder(`rfc${update.number}`)">
-                    <component :is="formatTitleAsVNode(`rfc${update.number}`)" />:
-                    {{ update.title }}
-                  </RFCRouterLink>
-                </span>
-              </dd>
-            </template>
-
-            <dt class="font-bold mt-2">Date published</dt>
-            <dd>{{ formattedPublished }}</dd>
-
-            <dt class="font-bold mt-2">Authors</dt>
-            <dd>
-              <ul class="-mt-1">
-                <li
-                  v-for="(author, authorIndex) in props.rfc.authors"
-                  :key="authorIndex"
-                  class="inline"
-                >
-                  <A
-                    v-if="author.email"
-                    :href="mailToBuilder(author.email)"
-                    class="whitespace-nowrap underline inline-block py-0.5 pr-0.5 mb-0.5"
-                  >
-                    {{ author.name }}
-                  </A>
-                  <span v-else>
-                    {{ author.name }}
-                  </span>
-                  <template v-if="authorIndex < props.rfc.authors.length - 1">
-                    {{ COMMA }}
-                    {{ NONBREAKING_SPACE }}
-                  </template>
-                  <template v-else>.</template>
-                </li>
-              </ul>
-            </dd>
-
-            <dt class="font-bold mt-2">Working group</dt>
-            <dd>
-              <template v-if="props.rfc.group.acronym">
-                {{ props.rfc.group.acronym.toUpperCase() }}
-              </template>
-              {{ props.rfc.group.name }}
-            </dd>
-
-            <dt class="font-bold mt-2">Area</dt>
-            <dd>
-              <template v-if="props.rfc.area?.acronym">
-                {{ props.rfc.area.acronym.toUpperCase() }}
-              </template>
-              {{ props.rfc.area?.name }}
-            </dd>
-
-            <dt class="font-bold mt-2">Stream</dt>
-            <dd>{{ props.rfc.stream.name }}</dd>
-
-            <template v-if="props.rfc.identifiers">
-              <template
-                v-for="(identifier, identifierIndex) in props.rfc.identifiers"
-                :key="identifierIndex"
-              >
-                <dt class="font-bold mt-2">
-                  <template v-if="identifier.type === 'doi'">
-                    <abbr
-                      title="Digital object identifier"
-                      class="no-underline"
-                    >
-                      DOI
-                    </abbr>
-                  </template>
-                  <template v-else-if="identifier.type === 'issn'">
-                    <abbr
-                      title="International Standard Serial Number"
-                      class="no-underline"
-                    >
-                      ISSN
-                    </abbr>
-                  </template>
-                  <template v-else>
-                    {{ identifier.type }}
-                  </template>
-                </dt>
-                <dd>
-                  {{ identifier.value }}
-                </dd>
-              </template>
-            </template>
-          </dl>
-
-          <!-- <Heading level="3" class="mt-5 mb-2">Cite this RFC</Heading>
-          <ul class="text-sm flex flex-col gap-2">
-            <li v-for="(citation, citationIndex) in props.rfc.citations" :key="citationIndex">
-              <A :href="citation.url" class="underline block px-2 -ml-2">
-                {{ citation.title }}
-              </A>
-            </li>
-          </ul> -->
-
-          <temmplate v-if="props.rfc.formats?.length > 0">
-            <Heading
-              level="3"
-              class="mt-5 mb-2"
-            >
-              Formats
-            </Heading>
-            <ul class="text-sm flex flex-col gap-2">
-              <li
-                v-for="(format, formatIndex) in props.rfc.formats"
-                :key="formatIndex"
-              >
-                <A
-                  :href="format"
-                  class="underline block px-2 -ml-2"
-                >
-                  {{ format }}
-                </A>
-              </li>
-            </ul>
-          </temmplate>
-        </template>
-        <template #slot3>
-          <ul class="text-sm">
-            <li
-              v-for="(errataItem, errataIndex) in props.rfc.errata"
-              :key="errataIndex"
-            >
-              {{ errataItem }}
-            </li>
-          </ul>
-        </template>
-      </RFCTabs>
+      />
     </div>
   </div>
 </template>
@@ -253,15 +61,7 @@ import {
   DialogTitle,
   DialogTrigger
 } from 'reka-ui'
-import { DateTime } from 'luxon'
-import { formatTitleAsVNode, type RfcBucketHtmlDocument, type RfcCommon } from '~/utilities/rfc'
-import { ANCHOR_TAILWIND_STYLE } from '~/utilities/theme'
-import { COMMA, NONBREAKING_SPACE, SPACE } from '~/utilities/strings'
-import {
-  infoRfcPathBuilder,
-  mailToBuilder,
-} from '~/utilities/url'
-import { formatDatePublished } from '~/utilities/rfc-converters-utils'
+import type { RfcBucketHtmlDocument, RfcCommon } from '~/utilities/rfc'
 import { closeModalAndScrollToId } from '~/utilities/tableOfContents'
 
 type Props = {
@@ -276,11 +76,6 @@ const isModalOpen = defineModel<boolean>('isModalOpen')
 const selectedTab = defineModel<number>('selectedTab')
 
 const props = defineProps<Props>()
-
-const formattedPublished = computed(() => {
-  const dt = DateTime.fromISO(props.rfc.published)
-  return formatDatePublished(dt, true)
-})
 
 const handleCloseAndNavigate = (id: string) => {
   isModalOpen.value = false
--- client/app/components/RFCTabs.vue
+++ client/app/components/RFCTabs.vue
@@ -1,72 +1,266 @@
 <template>
   <TabsRoot
     v-model="selectedTab"
-    class="min-h-0 flex flex-col"
+    class="min-h-0 flex flex-col text-black"
     @change="changeTab"
   >
-    <TabsList class="flex flex-row gap-5 border-b-2 border-gray-400">
-      <TabsIndicator class="absolute" />
-      <TabsTrigger
-        v-if="props.hasTableOfContents"
-        :class="[
-          DEFAULT_CLASS,
-          {
-            [SELECTED_CLASS]: selectedTab === 0,
-            [UNSELECTED_CLASS]: selectedTab !== 0
-          }
-        ]"
-        :value="0"
-      >
-        Contents
-      </TabsTrigger>
-      <TabsTrigger
-        :class="[
-          DEFAULT_CLASS,
-          {
-            [SELECTED_CLASS]: selectedTab === 1,
-            [UNSELECTED_CLASS]: selectedTab !== 1
-          }
-        ]"
-        :value="1"
-      >
-        About this RFC
-      </TabsTrigger>
-      <TabsTrigger
-        :class="[
-          DEFAULT_CLASS,
-          {
-            [SELECTED_CLASS]: selectedTab === 2,
-            [UNSELECTED_CLASS]: selectedTab !== 2
-          }
-        ]"
-        :value="2"
-      >
-        Erratum
-      </TabsTrigger>
+    <TabsList class="border-b-2 border-gray-400">
+      <HorizontalScrollable class="flex flex-row gap-5">
+        <TabsIndicator class="absolute" />
+        <TabsTrigger
+          v-if="props.hasTableOfContents"
+          :class="[
+            DEFAULT_CLASS,
+            {
+              [SELECTED_CLASS]: selectedTab === 0,
+              [UNSELECTED_CLASS]: selectedTab !== 0
+            }
+          ]"
+          :value="0"
+        >
+          Contents
+        </TabsTrigger>
+        <TabsTrigger
+          :class="[
+            DEFAULT_CLASS,
+            {
+              [SELECTED_CLASS]: selectedTab === 1,
+              [UNSELECTED_CLASS]: selectedTab !== 1
+            }
+          ]"
+          :value="1"
+        >
+          About this RFC
+        </TabsTrigger>
+        <TabsTrigger
+          :class="[
+            DEFAULT_CLASS,
+            {
+              [SELECTED_CLASS]: selectedTab === 2,
+              [UNSELECTED_CLASS]: selectedTab !== 2
+            }
+          ]"
+          :value="2"
+        >
+          Erratum
+        </TabsTrigger>
+      </HorizontalScrollable>
     </TabsList>
+
     <TabsContent
       v-if="props.hasTableOfContents"
       :value="0"
       :class="TAB_CONTENT_CLASS"
     >
-      <slot name="slot0" />
+      <TableOfContentsHighlight
+        v-if="props.isMobile === false && props.rfcBucketHtmlDocument.tableOfContents"
+        :toc="props.rfcBucketHtmlDocument.tableOfContents"
+        list-type="ordered"
+        wrapper-class="flex flex-col min-h-0 pt-4 pb-2 px-4"
+        list-class="mr-1"
+        nested-list-class="pl-2"
+        :links-class="`block text-sm py-2 dark:border-t-gray-500 ${ANCHOR_TAILWIND_STYLE}`"
+        links-active-class="text-shadow-bold"
+        link-class="block no-underline hover:underline"
+        last-link-class="flex-1"
+      >
+        <Heading
+          level="2"
+          style-level="5"
+          class="mt-4 mb-1 sr-only"
+        >
+          In this section
+        </Heading>
+      </TableOfContentsHighlight>
+      <TableOfContents
+        v-else-if="props.isMobile === true && props.rfcBucketHtmlDocument.tableOfContents"
+        :toc="props.rfcBucketHtmlDocument.tableOfContents"
+        list-type="ordered"
+        wrapper-class="flex flex-col min-h-0 pt-4 pb-2 px-4"
+        list-class="mt-2 mr-1 pl-0 -ml-1"
+        nested-list-class="pl-2"
+        :list-item-class="`block text-sm py-2 dark:border-t-gray-500 ${ANCHOR_TAILWIND_STYLE}`"
+        links-active-class="text-shadow-bold"
+        link-class="block no-underline hover:underline"
+        last-link-class="flex-1"
+      >
+        <Heading
+          level="2"
+          style-level="5"
+          class="mt-4 mb-1 sr-only"
+        >
+          In this section
+        </Heading>
+      </TableOfContents>
     </TabsContent>
     <TabsContent
       :value="1"
       :class="TAB_CONTENT_CLASS"
     >
-      <slot name="slot1" />
+      <Heading
+        level="3"
+        style-level="4"
+        class="mt-4"
+      >
+        Details
+      </Heading>
+      <dl class="text-sm">
+        <template v-if="rfc.updates && rfc.updates.length > 0">
+          <dt class="font-bold mt-2">Updates ({{ rfc.updates.length }})</dt>
+          <dd>
+            <span
+              v-for="(update, updateIndex) in props.rfc.updates"
+              :key="updateIndex"
+            >
+              <RFCRouterLink :href="infoRfcPathBuilder(`rfc${update.number}`)">
+                <component :is="formattedTitle" />:
+                {{ update.title }}
+              </RFCRouterLink>
+            </span>
+          </dd>
+        </template>
+
+        <dt class="font-bold mt-2">Date published</dt>
+        <dd>{{ formattedPublished }}</dd>
+
+        <dt class="font-bold mt-2">Authors</dt>
+        <dd>
+          <ul class="-mt-1">
+            <li
+              v-for="(author, authorIndex) in props.rfc.authors"
+              :key="authorIndex"
+              class="inline"
+            >
+              <A
+                v-if="author.email"
+                :href="mailToBuilder(author.email)"
+                class="whitespace-nowrap underline inline-block py-0.5 pr-0.5 mb-0.5"
+              >
+                {{ author.name }}
+                <Icon
+                  name="fluent:mail-32-regular"
+                  class="inline-block h-5 w-2"
+                />
+              </A>
+              <span v-else>
+                {{ author.name }}
+              </span>
+              <template v-if="authorIndex < props.rfc.authors.length - 1">
+                {{ COMMA }}
+                {{ NONBREAKING_SPACE }}
+              </template>
+            </li>
+          </ul>
+        </dd>
+
+        <dt class="font-bold mt-2">Working group</dt>
+        <dd>
+          <template v-if="props.rfc.group.acronym">
+            {{ props.rfc.group.acronym.toUpperCase() }}
+          </template>
+          {{ props.rfc.group.name }}
+        </dd>
+
+        <dt class="font-bold mt-2">Area</dt>
+        <dd>
+          <template v-if="props.rfc.area?.acronym">
+            {{ props.rfc.area.acronym.toUpperCase() }}
+          </template>
+          {{ props.rfc.area?.name }}
+        </dd>
+
+        <dt class="font-bold mt-2">Stream</dt>
+        <dd>{{ props.rfc.stream.name }}</dd>
+
+        <template v-if="props.rfc.identifiers">
+          <template
+            v-for="(identifier, identifierIndex) in props.rfc.identifiers"
+            :key="identifierIndex"
+          >
+            <dt class="font-bold mt-2">
+              <template v-if="identifier.type === 'doi'">
+                <abbr
+                  title="Digital object identifier"
+                  class="no-underline"
+                >
+                  DOI
+                </abbr>
+              </template>
+              <template v-else-if="identifier.type === 'issn'">
+                <abbr
+                  title="International Standard Serial Number"
+                  class="no-underline"
+                >
+                  ISSN
+                </abbr>
+              </template>
+              <template v-else>
+                {{ identifier.type }}
+              </template>
+            </dt>
+            <dd>
+              {{ identifier.value }}
+            </dd>
+          </template>
+        </template>
+      </dl>
+
+      <!-- <Heading level="3" class="mt-5 mb-2">Cite this RFC</Heading>
+          <ul class="text-sm flex flex-col gap-2">
+            <li v-for="(citation, citationIndex) in props.rfc.citations" :key="citationIndex">
+              <A :href="citation.url" class="underline block px-2 -ml-2">
+                {{ citation.title }}
+              </A>
+            </li>
+          </ul> -->
+
+      <template v-if="props.rfc.formats?.length > 0">
+        <Heading
+          level="3"
+          class="mt-5 mb-2"
+        >
+          Formats
+        </Heading>
+        <ul class="text-sm flex flex-col gap-2">
+          <li
+            v-for="(format, formatIndex) in props.rfc.formats"
+            :key="formatIndex"
+          >
+            <A
+              :href="format"
+              class="underline block px-2 -ml-2"
+            >
+              {{ format }}
+            </A>
+          </li>
+        </ul>
+      </template>
     </TabsContent>
     <TabsContent
       :value="2"
       :class="TAB_CONTENT_CLASS"
     >
-      <slot name="slot2" />
+      <ul
+        v-if="props.rfc.errata && props.rfc.errata.length > 0"
+        class="list-disc text-sm"
+      >
+        <li
+          v-for="(errataItem, errataIndex) in props.rfc.errata"
+          :key="errataIndex"
+        >
+          {{ errataItem }}
+        </li>
+      </ul>
+      <p
+        v-else
+        class="text-sm mt-5 lg:mt-5"
+      >No erratum currently.</p>
     </TabsContent>
   </TabsRoot>
 </template>
 
 <script setup lang="ts">
+import { DateTime } from 'luxon'
 import {
   TabsContent,
   TabsIndicator,
@@ -74,12 +268,17 @@ import {
   TabsRoot,
   TabsTrigger
 } from 'reka-ui'
-import type { RfcBucketHtmlDocument, RfcCommon } from '~/utilities/rfc'
+import { formatTitleAsVNode, type RfcBucketHtmlDocument, type RfcCommon } from '~/utilities/rfc'
+import { formatDatePublished } from '~/utilities/rfc-converters-utils'
+import { COMMA, NONBREAKING_SPACE } from '~/utilities/strings'
+import { ANCHOR_TAILWIND_STYLE } from '~/utilities/theme'
+import { infoRfcPathBuilder, mailToBuilder } from '~/utilities/url'
 
 type Props = {
   rfc: RfcCommon
-  rfcBucketHtmlDoc: RfcBucketHtmlDocument
+  rfcBucketHtmlDocument: RfcBucketHtmlDocument
   hasTableOfContents: boolean
+  isMobile: boolean
 }
 
 const props = defineProps<Props>()
@@ -90,8 +289,16 @@ function changeTab(index: number) {
   selectedTab.value = index
 }
 
-const TAB_CONTENT_CLASS = 'flex flex-col min-h-0'
-const DEFAULT_CLASS = 'py-4 whitespace-nowrap border-b-2 text-sm md:text-md'
+const formattedTitle = computed(() => formatTitleAsVNode(`rfc${props.rfc.number}`))
+
+
+const formattedPublished = computed(() => {
+  const dt = DateTime.fromISO(props.rfc.published)
+  return formatDatePublished(dt, true)
+})
+
+const TAB_CONTENT_CLASS = 'flex flex-col min-h-screen px-4'
+const DEFAULT_CLASS = 'py-4 px-2 whitespace-nowrap border-b-2 text-sm md:text-md'
 const SELECTED_CLASS =
   'text-shadow-bold text-gray-900 dark:text-gray-100 border-b-blue-900 dark:border-b-white font-medium'
 const UNSELECTED_CLASS = 'border-b-transparent text-gray-800 dark:text-gray-300'
