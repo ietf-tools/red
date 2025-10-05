3a53549750e32ff9ce460c18e79f1c52ad9cc4f1
Matthew Holloway <matthew@staff.ietf.org>
2025-10-06 10:50:42 +1300

    feat: track nuxt spa 'trackPageView' event

--- client/modules/analytics-matomo/runtime/client.ts
+++ client/modules/analytics-matomo/runtime/client.ts
@@ -1,3 +1,10 @@
+// intentionally not from a utility function
+declare global {
+  interface Window {
+    _paq?: (string | string[])[]
+  }
+}
+
 export default defineNuxtPlugin({
   setup(_nuxtApp) {
     useHead({
@@ -7,6 +14,27 @@ export default defineNuxtPlugin({
         }
       ]
     })
+    const route = useRoute()
+
+    watch(
+      route,
+      (value) => {
+        try {
+          let newUrl = new URL(value.fullPath, location.toString()).toString()
+          const matomoEventQueue = window._paq
+          if (Array.isArray(matomoEventQueue)) {
+            matomoEventQueue.push(['setCustomUrl', newUrl])
+            matomoEventQueue.push('trackPageView')
+            console.info('[PageView]', newUrl)
+          } else {
+            console.error('Unable to track page view due to lack of `window._paq` variable')
+          }
+        } catch (e) {
+          console.error('Analytics matomo error: ', e)
+        }
+      },
+      { deep: true, immediate: true }
+    )
   }
 })
 
