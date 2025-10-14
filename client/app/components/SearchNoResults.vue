ccc45fb1aad795598dff2eecc242a14aba3aaa5e
Matthew Holloway <matthew@staff.ietf.org>
2025-10-15 11:53:57 +1300

    fix: dark mode no results

--- client/app/components/SearchNoResults.vue
+++ client/app/components/SearchNoResults.vue
@@ -1,8 +1,12 @@
 <template>
   <div
-    class="rounded-xs bg-gray-200 text-gray-700 flex flex-col items-center justify-center px-3 py-5"
-    >
-    <Icon name="radix-icons:value-none" size="5em" class="text-gray-300" />
+    class="rounded-xs bg-gray-200 text-gray-700 dark:bg-gray-800 dark:text-gray-200 flex flex-col items-center justify-center px-3 py-5"
+  >
+    <Icon
+      name="radix-icons:value-none"
+      size="5em"
+      class="text-gray-300"
+    />
     <span class="mt-3">No RFCs match your search query and active filters.</span>
   </div>
 </template>
