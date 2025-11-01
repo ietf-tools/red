faae8da4ae557d697abe927050395f195b861141
Matthew Holloway <matthew@staff.ietf.org>
2025-11-02 04:11:07 +1300

    chore: use a promise pool for precomputer rfc generation (it's faster)

--- precomputer/src/all.ts
+++ precomputer/src/all.ts
@@ -1,24 +1,45 @@
+import { PromisePool } from '@supercharge/promise-pool'
+import { range } from 'lodash-es'
 import { uploadRfcData } from './tasks/rfc.ts'
-import { setTimeout } from 'node:timers/promises'
+
+const NUMBER_OF_CONCURRENT_RFC_PROCESSORS = 4
 
 const main = async (
   minRfcNumber: number,
   maxRfcNumber: number
 ): Promise<void> => {
-  for (let rfcNumber = minRfcNumber; rfcNumber <= maxRfcNumber; rfcNumber++) {
-    console.log(`Processing RFC ${rfcNumber}...`)
-    try {
-      const isDone = await uploadRfcData(rfcNumber)
-      if(isDone) {
-        console.log(`Pushed RFC ${rfcNumber} to bucket successfully.`)
-      } else {
-        console.error(`Unable to process RFC ${rfcNumber}`)
-      }
-      await setTimeout(80)
-    } catch (err) {
-      console.warn(`Failed to process ${rfcNumber}: ${(err as Error).message}`)
-    }
+  if (minRfcNumber >= maxRfcNumber) {
+    throw Error(
+      `Min RFC number (${minRfcNumber}) must be smaller than max RFC number (${maxRfcNumber})`
+    )
   }
+  const rfcRange = range(minRfcNumber, maxRfcNumber)
+
+  const { errors } = await PromisePool.for(rfcRange)
+    .withConcurrency(NUMBER_OF_CONCURRENT_RFC_PROCESSORS)
+    .process(async (rfcNumber, i) => {
+      console.log(`Processing RFC ${rfcNumber}...`)
+      try {
+        const isDone = await uploadRfcData(rfcNumber)
+        if (isDone) {
+          console.log(`Pushed RFC ${rfcNumber} to bucket successfully.`)
+        } else {
+          console.error(`Unable to process RFC ${rfcNumber}`)
+        }
+      } catch (err) {
+        console.warn(
+          `Failed to process ${rfcNumber}: ${(err as Error).message}`
+        )
+        throw err
+      }
+    })
+
+    if(errors.length > 0) {
+      console.error(errors)
+      process.exit(1)
+    } else {
+      process.exit(0)
+    }
 }
 
 if (!process.argv[2] || !process.argv[3]) {
