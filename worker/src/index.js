9bfc34a394c789b518755bd5e892ce7493d5df56
Robert Sparks <rjsparks@nostrum.com>
2026-04-15 13:13:46 -0500

    fix: remove leading C from cluster redirects

--- worker/src/index.js
+++ worker/src/index.js
@@ -58,7 +58,7 @@ router
   )
   .get('/cluster_info.php', (req) => {
     if (req.query?.cid?.startsWith('C')) {
-      return Response.redirect(`https://queue${env.ENV_DOMAIN}.rfc-editor.org/clusters/${req.query.cid}`, 302)
+      return Response.redirect(`https://queue${env.ENV_DOMAIN}.rfc-editor.org/clusters/${req.query.cid.slice(1)}`, 302)
     }
   })
   .get('/in-notes/prerelease/*', addNormalizedPath, (req) => {
@@ -73,7 +73,7 @@ router
   .get('/auth48/*', addNormalizedPath, (req) => {
     let match = req.normalizedPath.match(/^\/auth48\/c(?<num>\d+)$/i)
     if (match?.groups?.num) {
-      return Response.redirect(`https://queue${env.ENV_DOMAIN}.rfc-editor.org/final-review/C${match.groups.num}`, 302)
+      return Response.redirect(`https://queue${env.ENV_DOMAIN}.rfc-editor.org/final-review/${match.groups.num}`, 302)
     }
 
     match = req.normalizedPath.match(/^\/auth48\/rfc(?<num>\d+)$/i)
