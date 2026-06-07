683cbcca6d90d15b743abed0ef4a17e1b761d621
Matthew Holloway <matthew@staff.ietf.org>
2026-06-08 10:25:36 +1200

    fix: better handling of img alt text in markdown

--- website/app/components/content/ProseImg.vue
+++ website/app/components/content/ProseImg.vue
@@ -2,8 +2,17 @@
   <img
     v-bind="$attrs"
     class="dark:invert my-5"
+    :alt="alt"
   >
 </template>
 
 <script setup lang="ts">
+defineOptions({ inheritAttrs: false })
+const attrs = useAttrs()
+
+if (import.meta.dev && attrs.alt === undefined) {
+  console.warn('[ProseImg] Missing alt attribute on image:', attrs.src)
+}
+
+const alt = computed(() => typeof attrs.alt === 'string' ? attrs.alt : '')
 </script>
