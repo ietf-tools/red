18ceb391fa3553ec0934ad8364923238af12839c
Matthew Holloway <matthew@staff.ietf.org>
2026-06-11 15:27:45 +1200

    fix: don't use pre copy button on markdown pages

--- website/app/components/content/ProsePre.vue
+++ website/app/components/content/ProsePre.vue
@@ -1,13 +1,7 @@
 <template>
-  <PreCopyButton
+  <pre
     :class="`inline-block max-w-screen overflow-x-auto p-1 bg-gray-50 dark:bg-blue-950 ${
       '' // in <pre> make sure no whitespace before/after <slot />
-    }`"
-    ><slot
-  /></PreCopyButton>
+    }`"><slot
+  /></pre>
 </template>
-
-<script setup lang="ts">
-// <pre>formatted text can be wider than the page by not wrapping
-// so this ensures that wide <pre> styles don't break (eg) mobile layouts
-</script>
