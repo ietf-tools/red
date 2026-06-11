a88d85ec3ded02ebb585fb36274fe86813eca3fc
Matthew Holloway <matthew@staff.ietf.org>
2026-06-11 18:41:25 +1200

    fix: keyboard pilcrows

--- website/app/components/Main.vue
+++ website/app/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <main id="main" tabindex="0" class="flex-1 w-full mx-auto pb-10 visible focus:outline-0">
+  <main id="main" tabindex="-1" class="flex-1 w-full mx-auto pb-10 visible focus:outline-0">
     <slot />
   </main>
 </template>
