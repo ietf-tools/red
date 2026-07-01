32c7894452aebdf57a0752dd92d44bb98fed935e
Matthew Holloway <matthew@staff.ietf.org>
2026-07-02 10:53:29 +1200

    fix: markdown footer timestamp

--- website/app/components/ContentDocModifiedDateTime.vue
+++ website/app/components/ContentDocModifiedDateTime.vue
@@ -1,26 +1,8 @@
 <template>
-  <HoverCardRoot v-model:open="isHoverCardOpen">
-    <HoverCardTrigger as-child>
-      <button
-        class="mt-10 px-4 -ml-2 py-2 text-sm italic border border-transparent focus:border focus:border-gray-400 text-gray-600 dark:text-gray-400 rounded-md"
-        @focus="isHoverCardOpen = true"
-        @mouseover="isHoverCardOpen = true"
-        @blur="isHoverCardOpen = false"
-        :aria-expanded="isHoverCardOpen"
-        :aria-controls="domId">
-        Last updated {{ relativeDate }}
-      </button>
-    </HoverCardTrigger>
-    <HoverCardPortal>
-      <HoverCardContent
-        :id="domId"
-        class="border shadow-2xl overflow-x-hidden rounded-md bg-white dark:bg-black border-gray-400 dark:border-white px-3 py-2 data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade data-[state=open]:transition-all">
-        <p class="italic font-bold">Last updated {{ fullDate }}</p>
-        <HoverCardArrow class="fill-gray-200 stroke-gray-500 -mt-[1px]" />
-      </HoverCardContent>
-    </HoverCardPortal>
-  </HoverCardRoot>
-  <p class="hidden print:block italic font-semibold">Last updated {{ relativeDate }}, {{ fullDate }}</p>
+  <p class="mt-8 pl-2 italic font-semibold">
+    Last updated <time :datetime="isoTimeStamp">{{ relativeDate }}, on {{ fullDate }}</time
+    >.
+  </p>
 </template>
 
 <script setup lang="ts">
@@ -32,12 +14,10 @@ type Props = {
 
 const props = defineProps<Props>()
 
-const domId = useId()
-
 // eg. X years ago
 const relativeDate = computed(() => props.modifiedDateTime.toRelativeCalendar())
 
-const fullDate = computed(() => props.modifiedDateTime.toLocaleString(DateTime.DATETIME_FULL))
+const fullDate = computed(() => props.modifiedDateTime.toUTC().toFormat('d MMM yyyy'))
 
-const isHoverCardOpen = ref(false)
+const isoTimeStamp = computed(() => props.modifiedDateTime.toUTC().toISO() ?? undefined)
 </script>
