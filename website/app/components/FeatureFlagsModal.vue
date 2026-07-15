67fc41cb7179f183cc5bece14016388d001a2f17
Matthew Holloway <matthew@staff.ietf.org>
2026-07-16 11:41:15 +1200

    fix: feature flags modal glitch

--- website/app/components/FeatureFlagsModal.vue
+++ website/app/components/FeatureFlagsModal.vue
@@ -1,10 +1,9 @@
 <template>
   <DialogRoot v-model:open="isFeatureFlagsModalVisible">
     <DialogPortal>
-      <DialogOverlay
-        class="bg-blue-900/50 data-[state=open]:animate-overlayShow fixed inset-0 z-110 overflow-y-scroll py-16">
+      <DialogOverlay class="bg-blue-900/50 fixed inset-0 z-110 overflow-y-scroll py-16">
         <DialogContent
-          class="data-[state=open]:animate-contentShow mx-auto w-[90vw] max-w-[800px] relative rounded-md bg-white dark:bg-blue-900 px-5 pt-3 pb-5 z-[100]">
+          class="mx-auto w-[90vw] max-w-[800px] relative rounded-md bg-white dark:bg-blue-900 px-5 pt-3 pb-5 z-[100]">
           <DialogTitle as="h1" class="text-xl font-bold py-2 pr-16">
             RFC-Editor.org Feature Flag Experiments
           </DialogTitle>
