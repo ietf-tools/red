46b2736e926245051c981842cfdc8af375762c44
Matthew Holloway <matthew@staff.ietf.org>
2026-07-15 16:17:56 +1200

    fix: don't use localised timestamp message on an english website

--- website/app/components/ContentDocModifiedDateTime.vue
+++ website/app/components/ContentDocModifiedDateTime.vue
@@ -15,7 +15,11 @@ type Props = {
 const props = defineProps<Props>()
 
 // eg. X years ago
-const relativeDate = computed(() => props.modifiedDateTime.toRelativeCalendar())
+const relativeDate = computed(() =>
+  props.modifiedDateTime.toRelativeCalendar({
+    locale: 'en' // by default would be localised, but we should force English because the website's language is English, and switching to a localised language for just this one part would be odd (and would require lang attributes)
+  })
+)
 
 const fullDate = computed(() => props.modifiedDateTime.toUTC().toFormat('d MMMM yyyy'))
 
