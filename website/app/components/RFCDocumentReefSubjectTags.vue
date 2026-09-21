45b10b1a4a246194a9d9bd59875bfdf3b4f7b7b5
Matthew Holloway <matthew@staff.ietf.org>
2026-09-22 11:16:32 +1200

    fix: ux tweaks to info route rfc subject tags

--- website/app/components/RFCDocumentReefSubjectTags.vue
+++ website/app/components/RFCDocumentReefSubjectTags.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex ml-2 mt-2 mb-2">
+  <div class="flex mt-2 mb-2">
     <ul class="flex flex-col gap-4 reef:flex-row items-start pl-2 text-sm">
       <li v-for="subjectTag in props.reefSubjectTags" class="flex flex-col md:flex-row pr-2">
         <Anchor
           :href="subjectsPathBuilder(subjectTag.slug)"
           :class="[
-            'relative bg-blue-25 dark:bg-blue-800 px-1.5 py-0.5 no-underline rounded-r border-y border-r border-blue-50 dark:border-blue-300 font-bold',
+            'relative bg-blue-25 dark:bg-blue-800 px-1 py-0.5 no-underline rounded-r border-y border-r border-blue-50 dark:border-blue-300 font-bold',
             'before:content-[\'\'] before:absolute before:inset-y-0 before:right-full before:w-3 before:bg-blue-50 dark:before:bg-blue-300 before:[clip-path:polygon(0_50%,100%_0,100%_100%)]',
             'after:content-[\'\'] after:absolute after:top-px after:bottom-px after:right-full after:w-2.5 after:bg-blue-25 dark:after:bg-blue-800 after:border-r after:border-blue-25 dark:after:border-blue-700 after:[clip-path:polygon(0_50%,100%_0,100%_100%)]',
             'text-blue-300 dark:text-white hover:text-blue-500 dark:hover:text-blue-100'
